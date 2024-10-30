830b8fd6019d2cfe24fa5b3faab03e65d14414b7
Павел . <ozhigovpasha@gmail.com>
2024-10-30 16:20:21 +0700

    header_end

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-// src/components/Header.js
 import React from 'react';
 import '../styles/Header.css';
 
@@ -6,13 +5,36 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__logo">
-      <img src= {`${process.env.PUBLIC_URL}/images/Logo.png`} alt="Logo" />
+        <img src={`${process.env.PUBLIC_URL}/images/Logo.png`} alt="Logo" />
+        
+        <div className="header__location">
+          <img src={`${process.env.PUBLIC_URL}/images/Pin.png`} alt="Location Pin" />
+          NY, Manhattan
+        </div>
       </div>
-      <div className="header__location">NY, Manhattan</div>
+      
+      
+      <div className="header__track-wrapper">
+        <input 
+          type="text" 
+          className="header__track" 
+          placeholder="Track a Package" 
+        />
+        <img 
+          src={`${process.env.PUBLIC_URL}/images/search-icon.png`} 
+          alt="Search Icon" 
+          className="header__track-icon" 
+        />
+      </div>
+
+      {/* Навигация */}
       <nav className="header__nav">
-        <a href="/">Track a Package</a>
-        <a href="/">Send a Parcel</a>
-        <a href="/">Become a Courier</a>
+        <a href="/" className='nav_chld'>Send a Parcel</a>
+        <a href="/" className='nav_chld2'>Become a Courier</a>
+        <div className='log'> 
+          <img src={`${process.env.PUBLIC_URL}/images/Login.png`} alt='login'/> 
+        </div>
+        
       </nav>
     </header>
   );
